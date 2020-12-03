cbb7f82d5d72358bf6e5ca1385b3471bb42408fd
Fred Strauss <fred@strider.co.za>
2020-12-03 08:13:51 +0200

    Day 3

---
+++ 2020/day3.hs
@@ -0,0 +1,34 @@
+import System.IO
+
+type Pos    = (Int,Int)
+type Move   = (Int,Int)
+type Strat  = (Int,Int)
+type Topo   = [[Char]]
+
+applyMove :: Pos -> Move -> Pos
+applyMove (px,py) (mx,my) = (px+mx,py+my)
+
+-- return what's at pos on map, or 'X' if off bottom of map
+whatsHere :: Topo -> Pos -> Char
+whatsHere topo (x,y)
+  | y >= length topo  = 'X'
+  | otherwise         = topo!!y!!x'
+  where x'  = x `mod` length (topo!!y)
+
+treesHit :: Topo -> Strat -> Int
+treesHit topo strat = length $ filter (=='#') whatss
+  where moves   = repeat strat
+        poss    = scanl applyMove (0,0) moves
+        whatss  = takeWhile (/='X') $ map (whatsHere topo) poss
+
+main = do
+  topo <- lines <$> readFile "input3.txt"
+
+  --- Part One ---
+  putStrLn $ show $ treesHit topo (3,1)
+
+  --- Part Two ---
+  let strats = [(1,1), (3,1), (5,1), (7,1), (1,2)]
+  let hits = map (treesHit topo) strats
+
+  putStrLn $ show $ foldl1 (*) hits
