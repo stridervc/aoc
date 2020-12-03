becf013d8add7c290a6bf30a67b1b7d4c50ad009
Fred Strauss <fred@strider.co.za>
2020-12-03 14:59:46 +0200

    Change to using Maybe, suggested by /u/gilgamec

--- 2020/day3.hs
+++ 2020/day3.hs
@@ -1,3 +1,5 @@
+import Data.Maybe (isJust)
+
 type Pos    = (Int,Int)
 type Move   = (Int,Int)
 type Strat  = (Int,Int)
@@ -6,18 +8,19 @@ type Topo   = [[Char]]
 applyMove :: Pos -> Move -> Pos
 applyMove (px,py) (mx,my) = (px+mx,py+my)
 
--- return what's at pos on map, or 'X' if off bottom of map
-whatsHere :: Topo -> Pos -> Char
+-- return what's at pos on map, or Nothing if off bottom of map
+whatsHere :: Topo -> Pos -> Maybe Char
 whatsHere topo (x,y)
-  | y >= length topo  = 'X'
-  | otherwise         = topo!!y!!x'
+  | y >= length topo  = Nothing
+  | otherwise         = Just $ topo!!y!!x'
   where x'  = x `mod` length (topo!!y)
 
 -- how many trees do we hit with given movement strategy
 treesHit :: Topo -> Strat -> Int
-treesHit topo strat = length $ filter (=='#') whatss
+treesHit topo strat = length $ filter isTree whatss
   where poss    = scanl applyMove (0,0) $ repeat strat
-        whatss  = takeWhile (/='X') $ map (whatsHere topo) poss
+        whatss  = takeWhile isJust $ map (whatsHere topo) poss
+        isTree  = (\x -> x == Just '#')
 
 main = do
   topo <- lines <$> readFile "input3.txt"
