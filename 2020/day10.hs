120c8bafaad94dacd62647ede6a28bd3a656c5b9
Fred Strauss <fred@strider.co.za>
2020-12-10 14:17:54 +0200

    Day 10 Part 2, try tree (too slow)

--- 2020/day10.hs
+++ 2020/day10.hs
@@ -1,6 +1,8 @@
 import Data.List (sort)
+import Data.Tree
 
-addEndPoints is = 0 : is ++ [maximum is + 3]
+sanitise :: [Int] -> [Int]
+sanitise is = 0 : (sort is) ++ [maximum is + 3]
 
 count :: [Int] -> Int -> Int
 count is e = length $ filter (==e) is
@@ -11,16 +13,21 @@ countGaps is = map (count gaps) [0..3]
 
 part1 :: [Int] -> Int
 part1 input = gaps!!1 * gaps!!3
-  where input'  = addEndPoints $ sort input
-        gaps    = countGaps input'
+  where gaps  = countGaps input
 
 -- list of next valid adapters following 'j'
 nextValids :: [Int] -> Int -> [Int]
 nextValids js j = [n | n <- js, n-j>0, n-j<=3]
 
+treeify :: [Int] -> Int -> (Int, [Int])
+treeify xs start = (start, nextValids xs start)
+
 main = do
-  input <- map read <$> lines <$> readFile "input10.txt"
-  -- input <- map read <$> lines <$> readFile "example10.txt"
+  -- input <- sanitise <$> map read <$> lines <$> readFile "input10.txt"
+  input <- sanitise <$> map read <$> lines <$> readFile "example10.txt"
 
   print $ part1 input
 
+  let tree = unfoldTree (treeify input) 0
+  -- putStr $ drawTree $ fmap show tree
+  print $ foldTree (\_ xs -> if null xs then 1 else sum xs) tree
