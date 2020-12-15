14161720fc664862c5e099cb59b93a97cb61d2e8
Fred Strauss <fred@strider.co.za>
2020-12-15 09:50:33 +0200

    Change to record to also keep track of length

--- 2020/day15.hs
+++ 2020/day15.hs
@@ -6,40 +6,61 @@ import qualified Data.Map.Strict as M
 
 type Memo = M.Map Int Int
 
-next :: (Memo, [Int]) -> (Memo, [Int])
-next (memo, l@(x:xs)) =
-  case M.lookup x memo of
-    Nothing     -> (memo', 0 : l)
-    Just index  -> (memo', (length xs - index) : l)
-    where memo' = M.insert x (length xs) memo
+data Game = Game
+  { numbers :: [Int]
+  , latest  :: Int
+  , memo    :: M.Map Int Int
+  , numlen  :: Int
+  } deriving (Eq, Show)
 
-nextUntilLength :: Int -> (Memo, [Int]) -> (Memo, [Int])
-nextUntilLength len (memo, nums)
-  | length nums < len = nextUntilLength len $ next (memo, nums)
-  | otherwise         = (memo, nums)
+newGame :: [Int] -> Game
+newGame nums =
+  Game  { numbers = reverse $ init nums
+        , latest  = last nums
+        , memo    = M.fromList $ zip (init nums) [0..]
+        , numlen  = length nums - 1
+        }
 
-solve' :: Int -> [Int] -> (Memo, [Int])
-solve' count nums = nextUntilLength count (memo, nums)
-  where memo    = M.fromList $ zip (tail nums) indexes
-        indexes = reverse [0..length nums - 2]
+next :: Game -> Game
+next game =
+  Game  { numbers = latest' : xs
+        , latest  = nextnum
+        , memo    = newmemo
+        , numlen  = len + 1
+        }
+  where memo'   = memo game
+        latest' = latest game
+        len     = numlen game
+        xs      = numbers game
+        newmemo = M.insert latest' len memo'
+        nextnum = case M.lookup latest' memo' of
+          Nothing     -> 0
+          Just index  -> len - index
+
+nextUntilLength :: Int -> Game -> Game
+nextUntilLength len game
+  | len' < len  = nextUntilLength len $ next game
+  | otherwise   = game
+  where len'    = numlen game + 1
+
+solve' :: Int -> [Int] -> Game
+solve' count nums = nextUntilLength count $ newGame nums
 
 solve :: Int -> [Int] -> Int
-solve count nums = head $ snd $ solve' count nums
+solve count nums = latest $ solve' count nums
 
 part1 = solve 2020
 part2 = solve 30000000
 
 main :: IO ()
 main = do
-  let example1  = reverse [0,3,6]
-  let example2  = reverse [1,3,2]
-  let example3  = reverse [2,1,3]
-  let input     = reverse [0,1,5,10,3,12,19]
+  let example1  = [0,3,6]
+  let example2  = [1,3,2]
+  let example3  = [2,1,3]
+  let input     = [0,1,5,10,3,12,19]
 
   putStrLn "Part 1"
   testAndRun_ part1 [(example1, 436), (example2, 1), (example3, 10)] input
-
-  -- print $ solve' 7 example1
 
   putStrLn ""
   putStrLn "Part 2"
