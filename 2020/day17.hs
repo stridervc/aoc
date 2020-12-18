3aedad2f72583fe0c224cb7bd6a36be6bf8b393d
Fred Strauss <fred@strider.co.za>
2020-12-18 17:22:54 +0200

    Day 17 Part 1 works!

--- 2020/day17.hs
+++ 2020/day17.hs
@@ -1,8 +1,11 @@
+import AoC
+
 import Control.Monad (when)
 import Data.List.Split (chunksOf)
 import Text.Printf (printf)
 
 type ZSlice = [[Bool]]
+type Coord  = (Int, Int, Int)
 
 data ConwayCube = ConwayCube
   { cycles  :: !Int      -- the number of cycles this cube will support, determines max size
@@ -28,7 +31,7 @@ newConwayCube cycles input = ConwayCube
         w           = iw + 2*cycles
         h           = ih + 2*cycles
         d           = 1  + 2*cycles
-        -- TODO startslice needs padding on all 4 sides
+        -- startslice needs padding on all 4 sides
         tempslice   = map (map (== '#')) input
         padrows     = replicate cycles $ replicate w False
         padslice    = map (\r -> replicate cycles False ++ r ++ replicate cycles False) tempslice
@@ -38,7 +41,7 @@ newConwayCube cycles input = ConwayCube
 printConwayCube :: ConwayCube -> IO ()
 printConwayCube cube = do
   printf "Cycle %d / %d\n" (current cube) (cycles cube)
-  printf "Slices = %d\n\n" (length $ slices cube)
+  -- printf "Slices = %d\n\n" (length $ slices cube)
   mapM_ printSlice $ zip [0..] $ slices cube
   where printSlice (z, slice)  = when (or $ concat slice) (do
                                   putStr "z = "
@@ -54,12 +57,12 @@ actualCoords cube (x,y,z) = (x+c, y+c, z+c)
   where c = cycles cube
 -}
 
-getXYZ :: ConwayCube -> (Int, Int, Int) -> Bool
+getXYZ :: ConwayCube -> Coord -> Bool
 getXYZ cube (x,y,z) = slice !! y !! x
   where slice   = slices cube !! z
         w       = width cube
 
-setXYZ :: ConwayCube -> (Int, Int, Int) -> Bool -> ConwayCube
+setXYZ :: ConwayCube -> Coord -> Bool -> ConwayCube
 setXYZ cube (x,y,z) val = cube { slices = slices' }
   where slice   = slices cube !! z
         row     = slice !! y
@@ -67,24 +70,49 @@ setXYZ cube (x,y,z) val = cube { slices = slices' }
         slice'  = take y slice ++ [row'] ++ drop (y+1) slice
         slices' = take z (slices cube) ++ [slice'] ++ drop (z+1) (slices cube)
 
-getNeighbours :: ConwayCube -> (Int, Int, Int) -> [Bool]
+getNeighbours :: ConwayCube -> Coord -> [Bool]
 getNeighbours cube (x,y,z)  = map (getXYZ cube) ncoords
 -- getNeighbours :: ConwayCube -> (Int, Int, Int) -> [(Int,Int,Int)]
 -- getNeighbours cube (x,y,z)  = ncoords
   where offsets             = [-1,0,1]
-        validc (x',y',z')   = (x',y',z') /= (x,y,z) && x' >= 0 && y' >= 0 && z' >= 0 && x' < w+c && y' < h+c && z' < d
+        validc (x',y',z')   = (x',y',z') /= (x,y,z) && x' >= 0 && y' >= 0 && z' >= 0 && x' < w && y' < h && z' < d
         w                   = width cube
         h                   = height cube
         c                   = cycles cube
         d                   = depth cube
         ncoords             = filter validc [(x+x',y+y',z+z') | x' <- offsets, y' <- offsets, z' <- offsets]
 
+-- (Read only cube, writable cube)
+updateCell :: (ConwayCube, ConwayCube) -> Coord -> (ConwayCube, ConwayCube)
+updateCell (rc, wc) coord
+  | isalive   = if ncount == 2 || ncount == 3 then (rc, live) else (rc, die)
+  | otherwise = if ncount == 3 then (rc, live) else (rc, die)
+  where isalive     = getXYZ rc coord
+        neighbours  = getNeighbours rc coord
+        ncount      = length $ filter id neighbours
+        die         = setXYZ wc coord False
+        live        = setXYZ wc coord True
+
 step :: ConwayCube -> ConwayCube
-step cube = undefined
-  where coords  = [(x,y,z) | x <- [0..width cube - 1], y <- [0..height cube - 1], z <- [0..length (slices cube) -1]]
+step cube = cube' { current = curr' }
+  where coords        = [(x,y,z) | x <- [0..width cube - 1], y <- [0..height cube - 1], z <- [0..depth cube - 1]]
+        cube'         = snd $ foldl updateCell (cube, cube) coords
+        curr'         = current cube + 1
+
+run :: ConwayCube -> ConwayCube
+run cube
+  | current cube < cycles cube  = run $ step cube
+  | otherwise                   = cube
+
+countActive :: ConwayCube -> Int
+countActive cube = length $ filter id $ concat $ concat $ slices cube
+
+part1 :: [String] -> Int
+part1 = countActive . run . newConwayCube 6
 
 main :: IO ()
 main = do
-  input <- lines <$> readFile "example17.txt"
+  example <- lines <$> readFile "example17.txt"
+  input   <- lines <$> readFile "input17.txt"
 
-  printConwayCube $ newConwayCube 6 input
+  testAndRun_ part1 [(example,112)] input
