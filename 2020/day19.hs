a84f370d92527842c7fd13c59bb26f9a7e1e3c07
Fred Strauss <fred@strider.co.za>
2020-12-19 23:01:32 +0200

    Debug in progress for Day 19 Part 2

--- 2020/day19.hs
+++ 2020/day19.hs
@@ -68,16 +68,21 @@ parsedRule rules i  = case P.parse (ruleParser rules) ("(ruleParser rule " ++ sh
                         Left err    -> error $ show err
   where Just rule = M.lookup i rules
 
+-- false negative : bbbbbbbaaaabbbbaaabbabaaa
+-- 8: 42 | 42 8
+-- 11: 42 31 | 42 11 31
 satisfiesRules :: [Rule] -> String -> (Bool, String)
-satisfiesRules rules                    []      = (True, [])
+satisfiesRules rules                    []      = (null rules, [])
 satisfiesRules []                       cs      = (True, cs)
 satisfiesRules (RuleChar ch:rs)         (c:cs)  = let (ret, cs') = satisfiesRules rs cs in (c == ch && ret, cs')
 satisfiesRules (RuleOption rsl rsr:rs)  cs
-  | left      = satisfiesRules rs csl
-  | right     = satisfiesRules rs csr
-  | otherwise = (False, cs)
-  where (left, csl)   = satisfiesRules rsl cs
-        (right, csr)  = satisfiesRules rsr cs
+  | left && contleft    = (contleft, contcsl)
+  | right && contright  = (contright, contcsr)
+  | otherwise           = (False, cs)
+  where (left, csl)           = satisfiesRules rsl cs
+        (right, csr)          = satisfiesRules rsr cs
+        (contleft, contcsl)   = satisfiesRules rs csl
+        (contright, contcsr)  = satisfiesRules rs csr
 
 satisfiesRules (RuleSeq rules:rs)       cs      = (retseq && ret, cs')
   where (retseq, csseq) = satisfiesRules rules cs
@@ -92,10 +97,31 @@ part1 input = length $ filter id $ map (satisfiesRule (parsedRule rules 0)) msgs
   where Right (rulestrs, msgs)  = P.parse parseInput "" input
         rules                   = M.fromList rulestrs
 
+part2 :: String -> Int
+part2 input = length $ filter id $ map (satisfiesRule (parsedRule rules 0)) msgs
+  where Right (rulestrs, msgs)  = P.parse parseInput "" input
+        rules                   = M.insert 11 "42 31 | 42 11 31" $ M.insert 8 "42 | 42 8" $ M.fromList rulestrs
+
 main :: IO ()
 main = do
-  example <- readFile "example19.txt"
-  actual  <- readFile "input19.txt"
+  example1  <- readFile "example19.txt"
+  example2  <- readFile "example19b.txt"
+  actual    <- readFile "input19.txt"
 
-  testAndRun_ part1 [(example, 2)] actual
+  putStrLn "Part 1"
+  putStrLn "======"
+  testAndRun_ part1 [(example1, 2), (example2, 3)] actual
 
+  putStrLn ""
+  putStrLn "Debug Part 2"
+  putStrLn "============"
+  let Right (rulestrs, msgs) = P.parse parseInput "(example19b.txt)" example2
+  let rules = M.insert 11 "42 31 | 42 11 31" $ M.insert 8 "42 | 42 8" $ M.fromList rulestrs
+  mapM_ putStrLn $ filter (satisfiesRule (parsedRule rules 0)) msgs
+
+  putStrLn ""
+  putStrLn "Part 2"
+  putStrLn "======"
+  testAndRun_ part2 [(example2, 12)] actual
+
+  testAndRun_ part2 [] actual
